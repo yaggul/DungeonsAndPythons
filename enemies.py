11a93114f4fbb14321e048d90ae6877c04275c95
yaggul <krommul@gmail.com>
2015-12-14 21:15:53 +0200

    Adding more code to enemies.py

--- enemies.py
+++ enemies.py
@@ -1,3 +1,6 @@
+from random import randint
+
+
 class Enemy:
     def __init__(self, health=100, mana=100, damage=20):
         self.health = 100
@@ -5,16 +8,17 @@ class Enemy:
         self.damage = 20
 
     def is_alive(self):
-        return self.health > 0
+        if self.get_health():
+            return True
 
-    def can_cast(self):
-        self.mana > 0
+    def can_cast(self, spell_mana):
+        return self.mana > spell_mana > 0
 
     def get_health(self):
-        return self.health
+        print(self.health)
 
     def get_mana(self, mana_amount):
-        return self.mana
+        print(self.mana)
 
     def take_healing(self, health_amount):
         if self.health > 0:
@@ -35,7 +39,15 @@ class Enemy:
             print("Dead monsters do not need mana")
 
     def attack(self):
-        pass
+        self.take_healing(self.redeem())
+        self.take_mana(self.redeem())
+        return self.damage
 
-    def take_damage(self):
-        pass
+    def take_damage(self, hero):
+        if self.health - hero.attack() > 0:
+            self.health -= hero.attack()
+        else:
+            print("Enemy is DEAD")
+
+    def redeem(self):
+        return randint(1, self.damage)
