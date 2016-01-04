afac96e73a2fabc5c937feb605b093a7656dc5de
yaggul <krommul@gmail.com>
2016-01-04 22:58:47 +0200

    Ready the treasure.py

--- treasures.py
+++ treasures.py
@@ -5,3 +5,14 @@ class Treasure:
     def __init__(self):
         self.items = ['health_potion', 'mana_potion', 'weapon', 'spell']
 
+    def pick_item(self, *args):
+        return choice(self.items)
+
+
+def main():
+    tp = Treasure()
+    t = tp.pick_item()
+    print(t)
+
+if __name__ == '__main__':
+    main()
